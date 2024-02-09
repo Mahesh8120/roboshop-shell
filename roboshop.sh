6bb2a4389fb3c07948c787722e606519c8f258d0
Mahesh8120 <jonnalagaddamahi@gmail.com>
2024-02-09 22:46:08 +0530

    ansible

---
+++ roboshop.sh
@@ -0,0 +1,42 @@
+#!/bin/bang
+
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-04bb94f5d828fa09d
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "shipping" "payments" "cart" "catalogue" "user" "web" "dispatch")
+ZONE_ID=Z08780431GOB4T1TR5RPR
+DOMAIN_NAME=pjdevops.online
+for i in "${INSTANCES[@]}"
+do 
+   echo "instance is :$i"
+   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [  == "shipping" ]
+   then
+         INSTANCES_TYPE="t3.small"
+   else
+         INSTANCES_TYPE="t2.micro"
+   fi     
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        ' 
+done
+
+
+
