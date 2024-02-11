bc5d499db310c5d4037a89a6ea123aa66d4a1eb4
Mahesh8120 <jonnalagaddamahi@gmail.com>
2024-02-11 16:31:50 +0530

    prac

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME=pjdevops.online
 for i in "${INSTANCES[@]}"
 do 
    echo "instance is :$i"
-   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [  == "shipping" ]
+   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
    then
          INSTANCES_TYPE="t3.small"
    else
